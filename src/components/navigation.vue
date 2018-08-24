035af8dde93b3a8c3628cdcb8132cd62d7f72cd4
rishab2113 <rishab@bu.edu>
2018-08-24 21:29:31 +0530

    customized

--- src/components/navigation.vue
+++ src/components/navigation.vue
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-md navbar-light bg-light navbar-fixed">
   <div class="container">
     <a class="navbar-brand">
-      <router-link to="/">Bhav.AT</router-link>
+      <router-link to="/">Prantae</router-link>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -15,41 +15,14 @@
             <span class="sr-only">(current)</span>
           </a>
         </li>
-        <!--<li class="nav-item">
-              <a class="nav-link"><router-link :to="{ name: 'cities'}">Cities</router-link></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link"><router-link :to="{ name: 'workshops'}">Workshops</router-link></a>
-            </li>-->
         <li class="nav-item">
           <a class="nav-link">
             <router-link :to="{ name: 'allusers'}">Users</router-link>
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link">
-            <router-link :to="{ name: 'allprojects'}">Projects</router-link>
-          </a>
-        </li>
-        <!--<li class="nav-item">
-              <a class="nav-link"><router-link :to="{ name: 'news'}">News</router-link></a>
-            </li>-->
-        <!--<li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" id="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><router-link :to="{ name: 'about'}">About</router-link></a>
-              <div class="dropdown-menu" aria-labelledby="dropdown">
-                <a class="nav-link"><router-link :to="{ name: 'connect'}">Connect</router-link></a>
-                <a class="nav-link"><router-link :to="{ name: 'testimonials'}">Testimonials</router-link></a>
-              </div>
-            </li>-->
         <li v-if="user" class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">User Area</a>
           <div class="dropdown-menu" aria-labelledby="dropdown">
-            <a class="nav-link">
-              <router-link :to="{ name: 'myprojects'}">My Projects</router-link>
-            </a>
-            <a class="nav-link">
-              <router-link :to="{ name: 'allchats'}">Chat</router-link>
-            </a>
             <a class="nav-link">
               <router-link :to="{ path: profilelink()}">My Profile</router-link>
             </a>
